dbe2013389959e162755b5c6229eab73d65ce828
DaryaNori <daryanori100@gmail.com>
2026-09-13 17:05:40 +0330

    Update

---
+++ 2.0.Variables.py
@@ -0,0 +1,37 @@
+# variables_1
+
+x = 3 #This is a variable assignment.
+# print(x)
+
+# --------------
+# put another variable 
+a = 5
+a = "Hello, World!"
+# print(a) 
+
+# ---------------
+# type
+x = str(3)
+# print(x) 
+y = float(3)
+# print(y)
+b = 3
+# print(type(b))
+
+# ------------
+# Double qoutation & Single qoutation 
+c = "Darya code"
+# print(c)
+c = 'Darya code'
+# print(c)
+
+# ------------
+mystring = "Don't worry about apostrophes"
+# print(mystring)
+
+# ------------
+# case-sensitive
+m = 4
+M = "D"
+# print(m)
+# print(M)
